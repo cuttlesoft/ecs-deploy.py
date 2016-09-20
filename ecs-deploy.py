9d94cbeed94f0e4a6d1d93cc7c362bf2adb74060
Benji Shankwitz <bshankwitz@gmail.com>
2016-09-20 14:26:26 -0400

    Pull and register task_definition; Update service"

--- ecs-deploy.py
+++ ecs-deploy.py
@@ -14,7 +14,32 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def new_task_definition(family, definition, image):
+def get_task_definition(definition_name):
+    """
+    Pulls down existing taskDefinition
+    """
+    try:
+        client = boto3.client('ecs')
+    except ClientError as err:
+        print("Failed to create boto3 client.\n" + str(err))
+        return False
+
+    try:
+        """
+        see all parameters here:
+        https://boto3.readthedocs.io/en/latest/reference/services/ecs.html#\
+        ECS.Client.describe_task_definition
+        """
+        response = client.describe_task_definition(
+            taskDefinition=definition_name
+        )
+        return response['taskDefinition']
+    except ClientError as err:
+        print("Failed to retrieve task definition.\n" + str(err))
+        return False
+
+
+def new_task_definition(task_definition, image):
     """
     Registers a new ECS Task definition
     """
@@ -24,9 +49,7 @@ def new_task_definition(family, definition, image):
         print("Failed to create boto3 client.\n" + str(err))
         return False
 
-    with open(definition) as definition:
-        task_definition = json.load(definition)
-
+    family = task_definition['family']
     task_definition['image'] = image
     try:
         """
@@ -36,7 +59,7 @@ def new_task_definition(family, definition, image):
         """
         response = client.register_task_definition(
             family=family,
-            containerDefinitions=[task_definition]
+            containerDefinitions=task_definition['containerDefinitions']
         )
         return response['taskDefinition']['taskDefinitionArn']
     except ClientError as err:
@@ -50,8 +73,7 @@ def new_task_definition(family, definition, image):
         return False
 
 
-def update_service(cluster, service, count, task_definition, min_healthy,
-                   max_healthy):
+def update_service(cluster, service, task_definition):
     """
     Registers a new ECS Task definition
     """
@@ -70,15 +92,15 @@ def update_service(cluster, service, count, task_definition, min_healthy,
         response = client.update_service(
             cluster=cluster,
             service=service,
-            desiredCount=count,
-            taskDefinition=task_definition,
-            deploymentConfiguration={
-                # more information on the max and min healthy percentages
-                # can be found here:
-                # http://docs.aws.amazon.com/AmazonECS/latest/developerguide/update-service.html
-                'maximumPercent': max_healthy,
-                'minimumHealthyPercent': min_healthy
-            }
+            # desiredCount=count,
+            taskDefinition=task_definition
+            # deploymentConfiguration={
+            #     # more information on the max and min healthy percentages
+            #     # can be found here:
+            #     # http://docs.aws.amazon.com/AmazonECS/latest/developerguide/update-service.html
+            #     'maximumPercent': max_healthy,
+            #     'minimumHealthyPercent': min_healthy
+            # }
         )
         return response
     except ClientError as err:
@@ -91,32 +113,33 @@ def main():
     Your favorite wrapper's favorite wrapper
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "definition",
-        help="The path to an ECS Task Definition file in JSON format.  \
-        See this for details:  http://docs.aws.amazon.com/AmazonECS/\
-        latest/developerguide/task_definition_parameters.html#\
-        container_definitions"
-    )
+    parser.add_argument("cluster",
+                        help="The AWS cluster")
+    parser.add_argument("service",
+                        help="The AWS Service")
+    parser.add_argument("definition_name",
+                        help="The name of the AWS Task Definition")
     parser.add_argument("image",
                         help="The Docker Image to be used in the Task")
-    parser.add_argument("count",
-                        help="The desired number of Tasks to be running")
-    parser.add_argument("min_healthy", help="The minimum number of healthy \
-                        containers that should be running on the cluster")
-    parser.add_argument("max_healthy", help="The maximum number of healthy \
-                        containers that should be running on the cluster")
+    # parser.add_argument("count",
+    #                     help="The desired number of Tasks to be running")
+    # parser.add_argument("min_healthy", help="The minimum number of healthy \
+    #                     containers that should be running on the cluster")
+    # parser.add_argument("max_healthy", help="The maximum number of healthy \
+    #                     containers that should be running on the cluster")
     args = parser.parse_args()
 
-    cluster = os.getenv('ECS_CLUSTER_NAME')
-    service = os.getenv('ECS_SERVICE_NAME')
-    family = os.getenv('ECS_TASK_FAMILY_NAME')
-    task_definition = new_task_definition(family, args.definition, args.image)
+    # cluster = os.getenv('ECS_CLUSTER_NAME')
+    # service = os.getenv('ECS_SERVICE_NAME')
+    # family = os.getenv('ECS_TASK_FAMILY_NAME')
+    task_definition = get_task_definition(args.definition_name)
+    print(task_definition)
+
+    task_definition = new_task_definition(task_definition, args.image)
+    print(task_definition)
 
     if task_definition:
-        if not update_service(cluster, service, int(args.count),
-                              task_definition, int(args.min_healthy),
-                              int(args.max_healthy)):
+        if not update_service(args.cluster, args.service, args.definition_name):
             sys.exit(1)
     else:
         sys.exit(1)

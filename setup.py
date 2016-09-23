f8f889a6bb86f1c4399c2d802fdfa29779ff9275
Benji Shankwitz <bshankwitz@gmail.com>
2016-09-23 11:03:57 -0400

    Moto implemented and mocking some services

--- setup.py
+++ setup.py
@@ -12,75 +12,47 @@ def get_requirements(suffix=''):
 
 
 class PyTest(TestCommand):
+    test_package_name = 'ecs-deploy.py'
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = [
-            '-xrs',
-            '--cov', 'ecs-deploy.py',
-            '--cov-report', 'term-missing',
+        _test_args = [
+            '--verbose',
+            '--ignore=build',
+            '--cov={0}'.format(self.test_package_name),
+            '--cov-report=term-missing',
             '--pep8',
             '--cache-clear'
         ]
+        extra_args = os.environ.get('PYTEST_EXTRA_ARGS')
+        if extra_args is not None:
+            _test_args.extend(extra_args.split())
+        self.test_args = _test_args
         self.test_suite = True
 
     def run_tests(self):
         import pytest
+        from pkg_resources import normalize_path, _namespace_packages
+
+        if sys.version_info >= (3,) and getattr(self.distribution,
+                                                'use_2to3', False):
+            module = self.test_package_name
+            if module in _namespace_packages:
+                del_modules = []
+                if module in sys.modules:
+                    del_modules.append(module)
+                module += '.'
+                for name in sys.modules:
+                    if name.startswith(module):
+                        del_modules.append(name)
+                map(sys.modules.__delitem__, del_modules)
+
+            ei_cmd = self.get_finalized_command('egg_info')
+
+            self.test_args.append(normalize_path(ei_cmd.egg_base))
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
-
-
-# class PyTest(TestCommand):
-#     test_package_name = 'ecs-deploy.py'
-#
-#     def finalize_options(self):
-#         TestCommand.finalize_options(self)
-#         _test_args = [
-#             '--verbose',
-#             '--ignore=build',
-#             '--cov={0}'.format(self.test_package_name),
-#             '--cov-report=term-missing',
-#             '--pep8',
-#         ]
-#         extra_args = os.environ.get('PYTEST_EXTRA_ARGS')
-#         if extra_args is not None:
-#             _test_args.extend(extra_args.split())
-#         self.test_args = _test_args
-#         self.test_suite = True
-#
-#     def run_tests(self):
-#         import pytest
-#         from pkg_resources import normalize_path, _namespace_packages
-#
-#         # Purge modules under test from sys.modules. The test loader will
-#         # re-import them from the build location. Required when 2to3 is used
-#         # with namespace packages.
-#         if sys.version_info >= (3,) and getattr(self.distribution,
-#                                                 'use_2to3', False):
-#             # module = self.test_args[-1].split('.')[0]
-#             module = self.test_package_name
-#             if module in _namespace_packages:
-#                 del_modules = []
-#                 if module in sys.modules:
-#                     del_modules.append(module)
-#                 module += '.'
-#                 for name in sys.modules:
-#                     if name.startswith(module):
-#                         del_modules.append(name)
-#                 map(sys.modules.__delitem__, del_modules)
-#
-#             # Run on the build directory for 2to3-built code
-#             # This will prevent the old 2.x code from being found
-#             # by py.test discovery mechanism, that apparently
-#             # ignores sys.path..
-#             ei_cmd = self.get_finalized_command("egg_info")
-#
-#             # Replace the module name with normalized path
-#             # self.test_args[-1] = normalize_path(ei_cmd.egg_base)
-#             self.test_args.append(normalize_path(ei_cmd.egg_base))
-#
-#         errno = pytest.main(self.test_args)
-#         sys.exit(errno)
 
 
 tests_require = [
--- tests/test_something.py
+++ tests/test_something.py
@@ -4,10 +4,116 @@ from moto import mock_ecs
 
 
 @mock_ecs
-def test_something():
+def test_init():
     client = boto3.client('ecs')
 
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print(client.list_services(cluster='vanguard'))
+    mock_cluster = client.create_cluster(clusterName='mock_cluster')
 
-    assert False
+    mock_task = client.register_task_definition(
+        family='mock_task',
+        taskRoleArn='string',
+        networkMode='none',
+        containerDefinitions=[
+            {
+                'name': 'string',
+                'image': 'string',
+                'cpu': 123,
+                'memory': 123,
+                'memoryReservation': 123,
+                'links': [
+                    'string',
+                ],
+                'portMappings': [
+                    {
+                        'containerPort': 123,
+                        'hostPort': 123,
+                        'protocol': 'tcp'
+                    },
+                ],
+                'essential': True,
+                'entryPoint': [
+                    'string',
+                ],
+                'command': [
+                    'string',
+                ],
+                'environment': [
+                    {
+                        'name': 'string',
+                        'value': 'string'
+                    },
+                ],
+                'mountPoints': [
+                    {
+                        'sourceVolume': 'string',
+                        'containerPath': 'string',
+                        'readOnly': True
+                    },
+                ],
+                'volumesFrom': [
+                    {
+                        'sourceContainer': 'string',
+                        'readOnly': True
+                    },
+                ],
+                'hostname': 'string',
+                'user': 'string',
+                'workingDirectory': 'string',
+                'disableNetworking': True,
+                'privileged': True,
+                'readonlyRootFilesystem': True,
+                'dnsServers': [
+                    'string',
+                ],
+                'dnsSearchDomains': [
+                    'string',
+                ],
+                'extraHosts': [
+                    {
+                        'hostname': 'string',
+                        'ipAddress': 'string'
+                    },
+                ],
+                'dockerSecurityOptions': [
+                    'string',
+                ],
+                'dockerLabels': {
+                    'string': 'string'
+                },
+                'ulimits': [
+                    {
+                        'name': 'core',
+                        'softLimit': 123,
+                        'hardLimit': 123
+                    },
+                ],
+                'logConfiguration': {
+                    'logDriver': 'json-file',
+                    'options': {
+                        'string': 'string'
+                    }
+                }
+            },
+        ],
+        volumes=[
+            {
+                'name': 'string',
+                'host': {
+                    'sourcePath': 'string'
+                }
+            },
+        ]
+    )
+
+    mock_service = client.create_service(cluster='mock_cluster',
+                                         serviceName='mock_service',
+                                         taskDefinition='mock_task',
+                                         desiredCount=1)
+
+
+@mock_ecs
+def test_something():
+    # client.list_services(cluster='fake')
+    # conn = boto3.connect_ecs
+
+    assert True

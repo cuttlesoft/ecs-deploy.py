afcf0cab0673909c637f0d7303df0875edfb56cd
neillturner <neillwturner@gmail.com>
2018-04-18 14:10:29 +0100

    Support volumes, deregistering task, info logging, increase wait time increment

--- ecs_deploy.py
+++ ecs_deploy.py
@@ -176,13 +176,13 @@ class CLI(object):
 
         self.task_definition = self.client_fn('describe_task_definition')['taskDefinition']
 
-        logger.info("Deregister current ECS task definition " + self.task_definition['family'] +
+        logger.info("Deregister current ECS task definition " + self.task_definition['family'] + \
                                                        ':' + str(self.task_definition['revision']))
         self.client_fn('deregister_task_definition')['taskDefinition']
 
         logger.info("Registering new ECS task definition")
         self.new_task_definition = self.client_fn('register_task_definition')['taskDefinition']
-        logger.info("New ECS task definition " + self.new_task_definition['family'] + ':' +
+        logger.info("New ECS task definition " + self.new_task_definition['family'] + ':' + \
                                          str(self.new_task_definition['revision']) + ' Registered')
 
         if self.task_definition:
@@ -250,7 +250,7 @@ class CLI(object):
             kwargs['taskDefinition'] = self.task_definition_name
 
         elif fn == 'deregister_task_definition':
-            kwargs['taskDefinition'] = self.task_definition['family'] + ':' +
+            kwargs['taskDefinition'] = self.task_definition['family'] + ':' + \
                                              str(self.task_definition['revision'])
 
         elif fn == 'register_task_definition':
